64fbecdc68e2a7d3f15e26035963d53f4d054c14
Dacchu2803 <darshanj2803@gmail.com>
2025-05-29 19:34:38 +0530

    Code changes for Finish screen

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,15 @@ import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
+import FinishScreen from './FinishScreen';
 
 const initialState = {
   questions : [],
   status : "loading",
   index:0,
   answer:null,
-  points:0
+  points:0,
+  highscore:0
 };
 
 function reducer(state,action){
@@ -48,13 +50,19 @@ function reducer(state,action){
           ...state,
           index:state.index+1,
           answer:null
-        }
+        };
+      case "finish":
+        return{
+          ...state,
+          status:'finished',
+          highscore:state.points>state.highscore?state.points:state.highscore,
+        };
       default:
         throw new Error("Action unknown");
   }
 }
 export default function App(){
-  const [{questions,status,index,answer,points},dispatch] = useReducer(reducer,initialState);
+  const [{questions,status,index,answer,points,highscore},dispatch] = useReducer(reducer,initialState);
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev,curr)=>prev+curr.points,0);
   useEffect(function(){
@@ -73,7 +81,8 @@ export default function App(){
      <>
      <Progress index={index} numQuestions={numQuestions} points={points} maxPossiblePoints={maxPossiblePoints} answer={answer}/>
      <Question question={questions[index]} dispatch={dispatch} answer={answer}/>
-     <NextButton dispatch={dispatch} answer={answer}/></>}
+     <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions}/></>}
+     {status==="finished"&&<FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore}/>}
     </Main>
   </div>);
 }
